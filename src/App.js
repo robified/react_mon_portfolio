4993a30abbcafda935be99cf44664d65023e3324
Robin <daorob@gmail.com>
2019-10-16 20:03:44 -0500

    Import App.module.css|NavBar component, apply styles, and add NavBar component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
-import './App.css';
+import styles from './App.module.css';
+import NavBar from './components/NavBar/NavBar'
 
 export default class App extends Component {
     render() {
         return (
-            <div className="App App-header component">
-                App: I am an App component!
+            
+            <div className={`${styles.App} ${styles.component}`}>
+                <NavBar />
+                <p>App: I am an App component!</p>
             </div>
         )
     }
